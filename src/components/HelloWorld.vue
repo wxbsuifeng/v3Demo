ebf27c1b29de4a0c0b1f6849e11157a4661974d3
wxbsuifeng <1533569724@qq.com>
2021-03-18 23:09:12 +0800

    新特性尝试

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,18 +1,35 @@
 <template>
-  <h1>{{ msg }}</h1>
-  <button @click="count++">count is: {{ count }}</button>
-  <p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
+  <button @click="addFunc">count is: {{ count }}</button>
+  <p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.{{ count }}</p>
 </template>
 
 <script>
+import { onBeforeMount, onMounted, onBeforeUnmount, reactive, toRefs } from 'vue'
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
-  },
-  data() {
+  setup() {
+    const data = reactive({
+      count: 0,
+      addFunc: () => {
+        data.count++;
+      }
+    })
+    const refData = toRefs(data);
+
+    onBeforeMount(() => {
+      console.log('onBeforeMount');
+    })
+    
+    onMounted(() => {
+      console.log('onMounted----', data.count);
+    })
+
+    onBeforeUnmount(() => {
+      console.log('beforeUnmount----');
+    })
+
     return {
-      count: 0
+      ...refData
     }
   }
 }
---
+++ src/views/add/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+	import { reactive, toRefs, onBeforeMount, onMounted } from 'vue'
+
+	export default {
+		name: 'add',
+		setup() {
+			const data = reactive({
+
+			})
+			const refData = toRefs(data);
+
+			onBeforeMount(() => {
+			})
+
+			onMounted(() => {
+			})
+
+			return {
+				...refData,
+			}
+		}
+	};
+</script>
+<style scoped>
+</style>
